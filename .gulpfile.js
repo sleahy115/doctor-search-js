460be35c102a22b833f2bf2f8fd034c079e5ff85
Sarah Leahy <sleahy115@gmail.com>
2017-03-17 08:13:37 -0700

    add uglify, jshint, build and del tasks to gulpfile

--- .gulpfile.js
+++ .gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require("gulp");
 var source = require("vinyl-source-stream");
 var browserify = require("browserify");
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var utilities = require("gulp-util");
+var jshint = require("gulp-jshint");
+
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -14,4 +18,30 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+  gulp.start('cssBuild');
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
 });
