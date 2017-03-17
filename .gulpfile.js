64db7f21f429266ae13c544c86a59d2e6dd154ff
Sarah Leahy <sleahy115@gmail.com>
2017-03-17 08:10:11 -0700

    add browserify and concat to gulpfile

---
+++ .gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp");
+var source = require("vinyl-source-stream");
+var browserify = require("browserify");
+var concat = require("gulp-concat");
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
