eaea26e2c823e728dd65bee8045690811d9eefc6
Sarah Leahy <sleahy115@gmail.com>
2017-03-17 08:24:33 -0700

    add bower dependecies

--- .gulpfile.js
+++ .gulpfile.js
@@ -5,6 +5,20 @@ var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var utilities = require("gulp-util");
 var jshint = require("gulp-jshint");
+var del = require('del');
+var buildProduction = utilities.env.production;
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('concatInterface', function() {
@@ -44,4 +58,40 @@ gulp.task('build', ['clean'], function(){
 
 gulp.task("clean", function(){
   return del(['build', 'tmp']);
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
