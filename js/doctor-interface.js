616903bb4bd77db25cf9e450fcdad5ef437d87bb
sarah leahy <sarah leahy>
2017-03-16 22:29:29 -0600

    accept input from form in interface file

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,2 +1,12 @@
 var Doctor = require('./../js/doctor.js').DoctorModule
 var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function(){
+  $('.doctor-search').submit(function(event) {
+    event.preventDefault();
+    var ailment = $('#ailment').val();
+    var state = $('#state').val();
+    
+  });
+
+});
